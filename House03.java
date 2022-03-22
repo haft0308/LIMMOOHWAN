979de90368e673e6a6f6e059e1abeabedb057b5e
thesoop <101794458+thesoop@users.noreply.github.com>
2022-03-22 15:18:11 +0900

    Add files via upload

---
+++ House03.java
@@ -0,0 +1,45 @@
+
+public class House03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int a ;
+//		
+//		for( a=2; a<10; a++) {
+//			if(a >= 2)	{
+//				System.out.println();
+//			for(int i=1; i<10; i++ ) {
+//				System.out.print(a+"*"+i+"="+a*i+"\t");
+//				
+//					
+//				}
+//				
+//			}
+//
+//		}
+//		
+		
+		
+		
+		int a ;
+		
+		for( int i=1; i<10; i++) {
+			
+			
+			
+			for( a=2; a<10; a++ ) {
+				System.out.print(a+"*"+i+"="+a*i+"\t");
+			if(a >= 9)	
+				System.out.println();	
+					
+				}
+				
+			}
+
+		}
+		
+}		
+
+
+
+
