7d1e3a9cf0e9aca45f10dbea699e6b226e00c043
haft0308 <66650268+haft0308@users.noreply.github.com>
2022-03-22 15:21:38 +0900

    Update House03.java

--- House03.java
+++ House03.java
@@ -3,7 +3,9 @@ public class House03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int a ;
+//		//1)출력
+		
+		int a ;
 //		
 //		for( a=2; a<10; a++) {
 //			if(a >= 2)	{
@@ -21,6 +23,7 @@ public class House03 {
 		
 		
 		
+		//2)출력
 		int a ;
 		
 		for( int i=1; i<10; i++) {
